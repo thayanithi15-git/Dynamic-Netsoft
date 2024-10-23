6c6681ea264c3f4e84759379eacb394be0b70929
thayanithi15-git <thayanithi2006s@gmail.com>
2024-10-23 16:17:59 +0530

    added total adding functions

--- src/routers/routers.jsx
+++ src/routers/routers.jsx
@@ -1,13 +1,16 @@
-import React from 'react';
+import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Home from '../pages/home/home';
+import Home from "../pages/home/home";
+import { TotalProvider } from "../components/usecontext/usecontext";
 
 export default function Routers() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
-    </BrowserRouter>
-  )
+    <TotalProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </TotalProvider>
+  );
 }
